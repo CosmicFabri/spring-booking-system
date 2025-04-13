93c17d447f06c150737ea045695d60b72b242f38
CosmicFabri <fabrizzio86@outlook.es>
2025-04-13 12:11:43 -0600

    feat: The beginning.

---
+++ src/main/java/com/spring/spring_booking_system/SpringBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spring_booking_system/SpringBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
