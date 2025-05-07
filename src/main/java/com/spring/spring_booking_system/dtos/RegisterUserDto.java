d84aae2b824c92a804b1f95e72730d2ae4ec878d
Miguel <migtorruco@gmail.com>
2025-05-07 13:57:11 -0600

    feat: lombok added

--- src/main/java/com/spring/spring_booking_system/dtos/RegisterUserDto.java
+++ src/main/java/com/spring/spring_booking_system/dtos/RegisterUserDto.java
@@ -1,46 +1,15 @@
 package com.spring.spring_booking_system.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class RegisterUserDto {
     @JsonProperty("idRol")
     private Long roleId;
-
     private String email;
-
     private String password;
-
     private String fullName;
-
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 }
--- src/main/java/com/spring/spring_booking_system/entities/User.java
+++ src/main/java/com/spring/spring_booking_system/entities/User.java
@@ -5,11 +5,14 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import lombok.*;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 @Table(name = "users")
 @Entity
 public class User implements UserDetails {
@@ -67,61 +70,5 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
