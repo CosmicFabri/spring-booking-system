794c5eec0f04633f38ac6c65aaed4e880fd55027
CosmicFabri <fabrizzio86@outlook.es>
2025-05-08 19:31:43 -0600

    feat: Added endpoint protection for the spaces controller--the admin can perform those operations only.

--- src/main/java/com/spring/spring_booking_system/entities/User.java
+++ src/main/java/com/spring/spring_booking_system/entities/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import lombok.*;
 import java.util.Collection;
@@ -44,7 +45,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role.getDescription());
+
+        return List.of(authority);
     }
 
     @Override
