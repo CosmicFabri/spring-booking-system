25359e47df0b9d283c02e44f9d7a46f59d24e828
AleksPrz <alexis4g2@outlook.com>
2025-05-10 16:37:48 -0600

    fix: Changed "idRole" to "idRole"

--- src/main/java/com/spring/spring_booking_system/dtos/requests/RegisterRequest.java
+++ src/main/java/com/spring/spring_booking_system/dtos/requests/RegisterRequest.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterRequest {
-    @JsonProperty("idRol")
+    @JsonProperty("idRole")
     @NotNull(message = "Role ID is mandatory.")
     private Long roleId;
 
